a0dacc0ad5b3cbce91a268651f52fa492ee18c4f
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-03-19 22:31:43 -0500

    project setup/starter

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+interface HomeProps {}
+
+const Header = (props: HomeProps) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
