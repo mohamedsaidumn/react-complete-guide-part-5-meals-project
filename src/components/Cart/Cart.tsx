85ff3b2b45ea1c8062dd490cb63966a993dbd724
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-03-23 18:40:57 -0500

    customed CartItem and used CartItems from cart context instead of dummy items

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 interface CartProps {
   onClose: () => void;
 }
 
 const Cart = (props: CartProps) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount: string = `$ ${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems: boolean = cartCtx.items.length > 0;
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem name={item.name} amount={item.amount} price={item.price} />
       ))}
     </ul>
   );
@@ -20,13 +25,13 @@ const Cart = (props: CartProps) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
