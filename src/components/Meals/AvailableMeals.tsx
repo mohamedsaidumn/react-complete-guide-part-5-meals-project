28f227e0dd6b514b4b05e91a293a20f4fc797415
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-04-09 18:28:19 -0500

    error handling in the case of fetch request not succeding

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -13,15 +13,20 @@ interface MealItemType {
 const AvailableMeals = () => {
   const [meals, setMeals] = useState<MealItemType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-160f2-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
       console.log(responseData);
-
       const loadedMeals: MealItemType[] = [];
 
       for (const key in responseData) {
@@ -37,13 +42,24 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
